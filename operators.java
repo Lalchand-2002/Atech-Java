3242c0985660158cef26653ee0a591fd217b521c
Lalchand Hansda <jrhansda123@gmail.com>
2025-02-02 20:37:17 +0530

    update v1.0.0

---
+++ operators.java
@@ -0,0 +1,14 @@
+public class  {
+    public static void main(String[] a){
+        //Arithmetic operators
+        int num1=10,num2=5;
+        System.out.println(num1+num2);
+        System.out.println(num1-num2);
+        System.out.println(num1*num2);
+        System.out.println(num1/num2);
+        System.out.println(num1%num2);
+
+
+
+    }
+}
