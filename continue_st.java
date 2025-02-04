c10107b9a6b37846be96b7103f9f65c704466920
Lalchand Hansda <jrhansda123@gmail.com>
2025-02-04 23:17:59 +0530

    update v1.0.0

---
+++ continue_st.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class continue_st {
+    public static void main(String[] args) {
+        for (int i=0;i<=10;i++){
+            if (i==5){
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+}
---
+++ switch_break_st.java
@@ -0,0 +1,15 @@
+public class switch_break_st {
+    public static void main(String[] args) {
+        int i=1;
+        switch (i){
+            case 1:
+                System.out.println("Value is one");
+                break;
+            case 2:
+                System.out.println("Value is two");
+                break;
+            default:
+                System.out.println("No Value");
+        }
+    }
+}
